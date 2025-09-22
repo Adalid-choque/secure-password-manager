d10bdb7672042d1eee755b54c18d8be14a4f0c01
Adalid Choque <adalid.choque@ucb.edu.bo>
2025-09-22 13:39:26 -0400

    feat: crear templates HTML para gestión de contraseñas

--- password_manager/urls.py
+++ password_manager/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def home_redirect(request):
+    return redirect('list_passwords')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('passwords/', include('passwords.urls')),
+    path('', home_redirect, name='home'),
 ]
