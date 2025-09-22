9ddde6c35c869557f260a6ea0031f4a346233c6e
Adalid Choque <adalid.choque@ucb.edu.bo>
2025-09-22 15:40:29 -0400

    fix: restaurar configuración principal de URLs

--- password_manager/urls.py
+++ password_manager/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+from django.contrib.auth import views as auth_views
+
+def home_redirect(request):
+    return redirect('list_passwords')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('passwords/', include('passwords.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='admin/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', home_redirect, name='home'),
 ]
+
