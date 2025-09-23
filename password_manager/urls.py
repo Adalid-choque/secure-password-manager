54e567661bdf83b9c4c657f903efd8c373561f89
Adalid Choque <adalid.choque@ucb.edu.bo>
2025-09-23 15:17:12 -0400

    style: mejorar interfaz de usuario en login y listado de contraseñas

--- password_manager/urls.py
+++ password_manager/urls.py
@@ -25,7 +25,7 @@ def home_redirect(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('passwords/', include('passwords.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='admin/login.html'), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='passwords/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', home_redirect, name='home'),
 ]
